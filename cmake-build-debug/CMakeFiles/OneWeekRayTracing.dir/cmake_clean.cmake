9b592679d7d47210f0ce6251ac4dcf201001d06d
yvettemuki <373628977@qq.com>
2021-06-21 11:57:44 +0800

    initialize the c++ project

---
+++ cmake-build-debug/CMakeFiles/OneWeekRayTracing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OneWeekRayTracing.dir/main.cpp.o"
+  "OneWeekRayTracing"
+  "OneWeekRayTracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OneWeekRayTracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
