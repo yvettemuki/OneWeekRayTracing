890d1a67d914ec0188d68545c3c5bdb448fc59a2
yvettemuki <373628977@qq.com>
2021-10-17 13:34:50 -0400

    feat: all renew

--- color.h
+++ color.h
@@ -16,9 +16,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
 
     // Divide the color by the number of samples.
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    // Use gamma correction to light up the image 1/gamma as the power, here is 50% reflectors
+    // so gamma = 2
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     // Write the translated [0, 255] value of each color component.
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
--- rtweekend.h
+++ rtweekend.h
@@ -37,6 +37,13 @@ inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
+// Another implementation in random_double
+//inline double random_double() {
+//    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//    static std::mt19937 generator;
+//    return distribution(generator);
+//}
+
 inline double clamp(double x, double min, double max) {
     if (x < min) return min;
     if (x > max) return max;
